dc290704f05030b6c955bba7f1b76d3cad510c0e
David <0david0strobach0@gmail.com>
2025-11-25 14:10:43 +0100

    Replace font with Andika from Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Amaranth } from "next/font/google";
+import { Andika } from "next/font/google";
 import "./globals.css";
 
-const amaranth = Amaranth({
+const andika = Andika({
   weight: ["400", "700"],
-  variable: "--font-amaranth",
+  variable: "--font-andika",
   subsets: ["latin"],
 });
 
@@ -141,7 +141,7 @@ export default function RootLayout({
   return (
     <html lang="cs">
       <body
-        className={`${amaranth.variable} antialiased`}
+        className={`${andika.variable} antialiased`}
       >
         <script
           type="application/ld+json"
